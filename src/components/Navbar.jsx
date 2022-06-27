d8e499f0197ef7c1069092bf519baf254fe2d761
mikeydgithub <ampgzo2@gmail.com>
2022-06-27 19:30:26 -0400

    react app created and tested

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 60px;
+    background-color: black;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+        Navbar
+    </Container>
+  )
+}
+
+export default Navbar
