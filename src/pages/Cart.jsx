08386769f65df16d2edb86952ffa26d9d9523944
mikeydgithub <ampgzo2@gmail.com>
2022-07-01 14:38:06 -0400

    finish design of cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from "@mui/icons-material";
 import styled from "styled-components"
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
@@ -53,41 +54,117 @@ const Info = styled.div`
 `;
 
 const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 5px 0px;
 `;
 
 const ProductDetail = styled.div`
-
+    flex: 2;
+    display: flex;
 `;
 
-const Image = styled.div`
-    
+const Image = styled.img`
+    width: 200px;
 `;
 
 const Details = styled.div`
-    
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
 `;
 
-const ProductName = styled.div`
-    
+const ProductName = styled.span`
+    margin: 5px 0;
 `;
 
-const ProductId = styled.div`
-    
+const ProductId = styled.span`
+    margin: 5px 0;
 `;
 const ProductColor = styled.div`
-    flex: 1;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+    margin: 5px 0;
 `;
 
-const ProductSize = styled.div`
+const ProductSize = styled.span`
     flex: 1;
+    margin: 5px 0;
 `;
 
 const PriceDetail = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 `;
+
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+
+`;
+
+const ProductAmount = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`;
+
+const ProductPrice = styled.div`
+    font-size: 30px;
+    font-weight: 200;
+
+`;
+
+const Hr = styled.hr`
+    background-color: #eeee;
+    border: none;
+    height: 1px;
+`;
+
 
 const Summary = styled.div`
     flex: 1;
+    border: 0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    height: 50vh;
+    
+`;
+
+const SummaryTitle = styled.h1`
+    font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+    font-weight: ${props => props.type === "total" && "500"};
+    font-size: ${props => props.type === "total" && "24px"};
+
+`;
+
+const SummaryItemText = styled.span`
+
+`;
+
+const SummaryItemPrice = styled.span`
+
+`;
+
+const Button = styled.button`
+    width: 100%;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    font-weight: 600;
+    cursor: pointer;
 `;
 
 const Cart = () => {
@@ -113,21 +190,68 @@ const Cart = () => {
                     <ProductDetail>
                         <Image src="https://user-images.githubusercontent.com/94988620/176513508-26ea5e14-7089-4604-b7e5-b1f00b9ea962.jpg"/>
                         <Details>
-                            <ProductName><b>Product:</b> Blue Swim Suit</ProductName>
+                            <ProductName><b>Product:</b> Shirt</ProductName>
                             <ProductId><b>ID:</b> 84848488484</ProductId>
-                            <ProductColor/>
-                            <ProductSize><b>Size:</b>37.5</ProductSize>
+                            <ProductColor color="beige"/>
+                            <ProductSize><b>Size:</b> Large</ProductSize>
                         </Details>
                     </ProductDetail>
                     <PriceDetail>
                         <b>Price:</b>
+                        <ProductAmountContainer>
+                            <Add/>
+                            <ProductAmount>2</ProductAmount>
+                            <Remove/>
+                        </ProductAmountContainer>
+                        <ProductPrice>$30</ProductPrice>
+                    </PriceDetail>
+                </Product>
+                <Hr/>
+                <Product>
+                    <ProductDetail>
+                        <Image src="https://user-images.githubusercontent.com/94988620/176513508-26ea5e14-7089-4604-b7e5-b1f00b9ea962.jpg"/>
+                        <Details>
+                            <ProductName><b>Product:</b> Blue Swim Suit</ProductName>
+                            <ProductId><b>ID:</b> 84848488483</ProductId>
+                            <ProductColor color="beige"/>
+                            <ProductSize><b>Size:</b> Medium</ProductSize>
+                        </Details>
+                    </ProductDetail>
+                    <PriceDetail>
+                        <b>Price:</b>
+                        <ProductAmountContainer>
+                            <Add/>
+                            <ProductAmount>2</ProductAmount>
+                            <Remove/>
+                        </ProductAmountContainer>
+                        <ProductPrice>$30</ProductPrice>
                     </PriceDetail>
                 </Product>
             </Info>
-            <Summary>Summary</Summary>
+            <Summary>
+                <SummaryTitle>
+                    Order Summary
+                </SummaryTitle>
+                <SummaryItem>
+                    <SummaryItemText>Subtotal:</SummaryItemText>
+                    <SummaryItemPrice>$80</SummaryItemPrice>
+                </SummaryItem>
+                <SummaryItem>
+                    <SummaryItemText>Estimated Shipping</SummaryItemText>
+                    <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+                </SummaryItem>
+                <SummaryItem>
+                    <SummaryItemText>Shipping Discount</SummaryItemText>
+                    <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+                </SummaryItem>
+                <SummaryItem type="total">
+                    <SummaryItemText>Total</SummaryItemText>
+                    <SummaryItemPrice>$ 80</SummaryItemPrice>
+                </SummaryItem>
+                <Button>Checkout Now</Button>
+            </Summary>
         </Bottom>
     </Wrapper>
-    cart
     <Footer/>
     </Container>
   )
